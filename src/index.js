8ecfe7d7107ea606c55f71bce1a0a2fe1c4b9d57
john-kelly <johnkellytemena@gmail.com>
2025-04-09 19:57:15 +0800

    Add dropdown demo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import "./styles.css";
+import "./styles/global.css";
+import { dropdown } from "./modules/dropdown";
+
+let dropdownHover = document.querySelector("#hover");
+let dropdownClick= document.querySelector("#click");
+let dropdownLeft= document.querySelector("#left");
+let dropdownRight= document.querySelector("#right");
+let dropdownCenter= document.querySelector("#center");
+
+dropdown(dropdownHover, "hover");
+dropdown(dropdownClick, "click");
+dropdown(dropdownLeft, "hover"); // left aligned by default
+dropdown(dropdownRight, "hover", "right");
+dropdown(dropdownCenter, "hover", "center");
+
